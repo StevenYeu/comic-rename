937d97b9ff13278519e8f6800757301dd6c76b89
Steven Yeu <stevenhanhyeu@gmail.com>
2024-11-24 11:15:08 -0800

    update regex to do a findall instead of serach to return all match groups

--- comic.py
+++ comic.py
@@ -1,9 +1,17 @@
 import re
-import sys
 from os import listdir, rename
 from os.path import isfile, join
+import argparse
 
-mypath = sys.argv[1]
+parser = argparse.ArgumentParser(
+    prog="Comic Rename",
+    description="Rename comics files to 'Name Vol Num' format",
+)
+parser.add_argument("src")
+parser.add_argument("-d", "--dry", action="store_true")
+args = parser.parse_args()
+directory_path: str = args.src
+is_dry_run: bool = args.dry
 
 patternSpaceParen = r"(\s*)\((.*?)\)(\s*)"
 regexSpaceParen = re.compile(patternSpaceParen)
@@ -14,16 +22,26 @@ regexVol = re.compile(patternVol)
 patternVolNum = r"(?!v)(\d+)"
 regexVolNum = re.compile(patternVolNum)
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+onlyfiles = [f for f in listdir(directory_path) if isfile(join(directory_path, f))]
 for f in onlyfiles:
-    originalFile = join(mypath, f)
+    if not f.endswith(".cbr") and not f.endswith(".cbz"):
+        continue
+    originalFile = join(directory_path, f)
     newName = regexSpaceParen.sub("", f)
-    volNum = regexVolNum.search(f)
-    replacement = "Vol. " + volNum.group(0)
+    matchList = regexVolNum.findall(newName)
+    if len(matchList) == 0:
+        continue
+    volNum = matchList[-1]
+    replacement = "Vol. " + volNum
     finalName = regexVol.sub(replacement, newName)
-    newFile = join(mypath, finalName)
-    print(f"Renaming {f} to {finalName}")
-    try:
-        rename(originalFile, newFile)
-    except FileNotFoundError:
-        print(f"Can't find {f}")
+    newFile = join(directory_path, finalName)
+    if not is_dry_run:
+        print(f"Renaming {f} to {finalName}")
+        try:
+            rename(originalFile, newFile)
+        except FileNotFoundError:
+            print(f"Can't find {f}")
+    else:
+        print("DRY RUN")
+        print(f"Renaming {f} to {finalName}")
+        print("END OF DRY RUN")
